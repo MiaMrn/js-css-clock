c9405fc30658223f5c0096a0c3421ed2c1c6828d
Marie Morin <mariemorin19@gmail.com>
2020-06-17 19:45:20 +0200

    Mise en place fonctionnement de l'horloge en js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("hello");
+function getHour() {
+    const date = new Date();
+
+    const seconds = date.getSeconds();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+
+    let rotateSec = seconds * 6 + 90;
+    let rotateMin = minutes * 6 + 90;
+    let rotateHou = hours * 30 + 90;
+
+    let secondsElt = document.querySelector(".seconds");
+    secondsElt.style.transform = `translate(-50%, -50%) rotate(${rotateSec}deg)`;
+
+    let minutesElt = document.querySelector(".minutes");
+    minutesElt.style.transform = `translate(-50%, -50%) rotate(${rotateMin}deg)`;
+
+    let hoursElt = document.querySelector(".hours");
+    hoursElt.style.transform = `translate(-50%, -50%) rotate(${rotateHou}deg)`;
+}
+
+window.setInterval(getHour, 1000);
